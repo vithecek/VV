7f8385c98f988a0b8e1f4cdb81545940605b17f6
vitecek <vinickyvitek@icloud.com>
2026-03-18 11:36:18 +0100

    180326

---
+++ 3.3.2/script.js
@@ -0,0 +1,60 @@
+let userName = "Tomáš";
+console.log(userName)
+ 
+userName = "Honzík";
+console.log(userName)
+ 
+console.log(7+4)
+console.log("7"+4)
+console.log("7"+"4")
+console.log("A"+"B")
+ 
+let age = 31;
+console.log(age > 20);
+ 
+let text = "Text"
+console.log(typeof text)
+ 
+let a;
+console.log(a)
+let b = null;
+console. log(null)
+ 
+let isLoggedIn = false;
+console.log(typeof isLoggedIn)
+ 
+// Aritmické operace
+ 
+let čísloA = 10;
+let čísloB = 5;
+console.log( čísloA + čísloB);
+console.log( čísloA - čísloB);
+console.log( čísloA * čísloB);
+console.log( čísloA / čísloB);
+ 
+// Umocnování
+ 
+console.log( čísloA ** čísloB);
+ 
+// zbytek - modulo
+ 
+console.log(čísloA % čísloB);
+ 
+console.log(čísloA + čísloB * čísloB);
+ 
+console.log((čísloA + čísloB) * čísloB);
+ 
+// Aktualisace promněné (přiřazovací oparátory)
+ 
+let number = 18;
+number = number + 1;
+number += 1;
+number -= 2;
+number *= 4;
+number /= 6;
+number %= 3;
+number++;
+number--;
+ 
+console.log(number)
+ 
---
+++ 3.3.3/script.js
@@ -0,0 +1,7 @@
+let age = 30;
+ 
+if (age > 20) {
+    console.log("Věk je větší než 20.")
+} else {
+    console.log("Věk je 20 nebo méně.")
+}
